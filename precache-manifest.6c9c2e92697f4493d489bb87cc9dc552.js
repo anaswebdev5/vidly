101f0e69596e05024913e8907c14630883efcfad
anaswebdev5 <anaswebdev5@gmail.com>
2020-09-16 09:27:01 +0300

    Updates

---
+++ precache-manifest.6c9c2e92697f4493d489bb87cc9dc552.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e0c4daa3da626ce93c4191aa405cacf",
+    "url": "/anaswebdev5/vidly/index.html"
+  },
+  {
+    "revision": "8c4d57fdd53c43d512ef",
+    "url": "/anaswebdev5/vidly/static/css/2.c6b493a5.chunk.css"
+  },
+  {
+    "revision": "dca50cd63d3b9be8e6fb",
+    "url": "/anaswebdev5/vidly/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8c4d57fdd53c43d512ef",
+    "url": "/anaswebdev5/vidly/static/js/2.ad8b473d.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/anaswebdev5/vidly/static/js/2.ad8b473d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dca50cd63d3b9be8e6fb",
+    "url": "/anaswebdev5/vidly/static/js/main.e41022d5.chunk.js"
+  },
+  {
+    "revision": "dfec1738e7047bc05c98",
+    "url": "/anaswebdev5/vidly/static/js/runtime-main.7df998e8.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/anaswebdev5/vidly/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/anaswebdev5/vidly/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/anaswebdev5/vidly/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/anaswebdev5/vidly/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/anaswebdev5/vidly/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
